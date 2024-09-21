82a8d7209da66483aa75a4a67c3f45abc0f219d4
Ramerlin <Ramerlint2008@gmail.com>
2024-09-21 16:54:29 -0400

    Pre hecho

---
+++ Registro.py
@@ -0,0 +1,113 @@
+from PyQt6.QtWidgets import (QDialog,QLabel,QPushButton,QLineEdit,QMessageBox)
+from PyQt6.QtGui import QFont
+
+class RegistroDeUsuario(QDialog):
+    
+    def __init__(self):
+        super().__init__()
+        self.setModal(True)
+        self.formulario_segundo()
+        
+    def formulario_segundo(self):
+        self.setGeometry(200,200,400,400)
+        self.setWindowTitle("Registro")
+        
+        usuario_label = QLabel(self)
+        usuario_label.setText("Nombre")
+        usuario_label.setFont(QFont("Comic Sans MS",11))
+        usuario_label.move(55,54) 
+        
+        self.usuario_input = QLineEdit(self)
+        self.usuario_input.resize(270,35)
+        self.usuario_input.move(50,80)
+        self.usuario_input.setFont(QFont("Comic Sans MS",11))
+        self.usuario_input.setPlaceholderText("Ingrese un Nombre de usuario")
+        
+        password1_label = QLabel(self)
+        password1_label.setText("Ingrese una contraseña")
+        password1_label.setFont(QFont("Comic Sans MS",11))
+        password1_label.move(55,125) 
+        
+        self.password1_input = QLineEdit(self)
+        self.password1_input.resize(270,35)
+        self.password1_input.move(50,150)
+        self.password1_input.setFont(QFont("Comic Sans MS",11))
+        self.password1_input.setPlaceholderText("Ingrese una contraseña")
+        self.password1_input.setEchoMode(
+            QLineEdit.EchoMode.Password
+        )
+        
+        password2_label = QLabel(self)
+        password2_label.setText("Confirmar contraseña")
+        password2_label.setFont(QFont("Comic Sans MS",11))
+        password2_label.move(55,197) 
+        
+        self.password2_input = QLineEdit(self)
+        self.password2_input.resize(270,35)
+        self.password2_input.move(50,220)
+        self.password2_input.setFont(QFont("Comic Sans MS",11))
+        self.password2_input.setPlaceholderText("Confirmar Contraseña")
+        self.password2_input.setEchoMode(
+            QLineEdit.EchoMode.Password
+        )
+        
+        crear_btn = QPushButton(self)
+        crear_btn.setText("Registrarse")
+        crear_btn.resize(150,40)
+        crear_btn.move(110,270)
+        crear_btn.setFont(QFont("Segoe UI",11))
+        crear_btn.clicked.connect(self.Registrarse)
+        crear_btn.setStyleSheet("""
+            QPushButton {
+                background-color: #3c3c3c;    
+                border-radius: 15px;          
+                color: white;                 
+                font-size: 16px;              
+                padding:5px;     
+                border-bottom: 0.5px solid #727272;  
+            
+            }
+            QPushButton:hover {
+                background-color: #424242;   
+            }
+            QPushButton:pressed {
+                background-color: #4E4E4E; 
+                color: #979696;   
+            }
+        """)
+        
+        btn_atras = QPushButton(self)
+        btn_atras.setText("<-") #!Poner Icono mas adelante 21-9-24
+        btn_atras.resize(50,35)
+        btn_atras.move(10,2)
+        btn_atras.setFont(QFont("Comic Sans MS",30))
+        btn_atras.setStyleSheet("background-color: transparent; border: none;")
+        btn_atras.clicked.connect(self.Volver_atras)
+        
+    def Volver_atras(self):
+        self.close()
+        
+        
+    def Registrarse(self):
+        Usuario = self.usuario_input.text()
+        Password1 = self.password1_input.text()
+        Password2 = self.password2_input.text()
+        
+        if Usuario == "" or Password1 == "" or Password2 == "":
+            QMessageBox.warning(self,"Error","Los campos no pueden estar vacios",QMessageBox.StandardButton.Close,QMessageBox.StandardButton.Close)
+        elif not Usuario.isalnum():
+            QMessageBox.warning(self,"Error","El nombre no puede contener ningun caracter especial",QMessageBox.StandardButton.Close,QMessageBox.StandardButton.Close)
+        elif Password1 == Password2:
+            QMessageBox.information(self,"Completado","Se registro correctamente!",QMessageBox.StandardButton.Ok,QMessageBox.StandardButton.Ok)
+            self.close()
+        elif Password1 != Password2:
+            QMessageBox.warning(self,"Error","Las contraseñas no coinciden",QMessageBox.StandardButton.Close,QMessageBox.StandardButton.Close)
+                
+
+                    
+            
+                
+                
+        
+        
+        
---
+++ login.py
@@ -0,0 +1,127 @@
+import sys
+from PyQt6.QtWidgets import QApplication,QWidget,QLabel,QLineEdit,QPushButton,QMessageBox,QCheckBox
+from PyQt6.QtGui import QFont, QPixmap
+from Registro import RegistroDeUsuario
+
+
+class Login(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        self.inicializadorUI()
+        
+    def inicializadorUI(self):
+        self.setGeometry(200,200,400,400)
+        self.setWindowTitle("Login")
+        self.generarFormulario()
+        self.show()
+        
+    def generarFormulario(self):
+        self.is_logged = False
+        
+        user_label = QLabel(self)
+        user_label.setText("Usuario")
+        user_label.setFont(QFont("Comic Sans MS",11))
+        user_label.move(20,59)
+        
+        self.user_input = QLineEdit(self)
+        self.user_input.resize(250,35)
+        self.user_input.move(90,50)
+        self.user_input.setPlaceholderText("Ingrese su usuario")
+        self.user_input.setFont(QFont("Comic Sans MS",11))#Segoe UI
+        
+        password_label = QLabel(self)
+        password_label.setText("Password")
+        password_label.setFont(QFont("Comic Sans MS",11))
+        password_label.move(15,122)
+        
+        self.password_input = QLineEdit(self)
+        self.password_input.resize(250,35)
+        self.password_input.move(90,115)
+        self.password_input.setEchoMode(
+             QLineEdit.EchoMode.Password
+         )
+        self.password_input.setPlaceholderText("Ingrese su contraseña")
+        self.password_input.setFont(QFont("Comic Sans MS",11))
+        
+        #self.check_vista_password = QCheckBox(self)
+        #self.check_vista_password.setText("Ver Contraseña")
+        #self.check_vista_password.move(95,155)
+       # self.check_vista_password.toggled.connect(self.mostrar_contraseña_check)
+        
+         
+        login_button = QPushButton(self)
+        login_button.setText("Login")
+        login_button.resize(100,40)
+        login_button.move(90,190)
+        login_button.setFont(QFont("Segoe UI",11))
+        login_button.clicked.connect(self.iniciar_mainview)
+        login_button.setStyleSheet("""
+            QPushButton {
+                background-color: #3c3c3c;    
+                border-radius: 15px;          
+                color: white;                 
+                font-size: 16px;              
+                padding:5px;     
+                border-bottom: 0.5px solid #727272;  
+            
+            }
+            QPushButton:hover {
+                background-color: #424242;   
+            }
+            QPushButton:pressed {
+                background-color: #4E4E4E; 
+                color: #979696;   
+            }
+        """)
+        
+        
+        register_button = QPushButton(self)
+        register_button.setText("Registrate")
+        register_button.resize(100,40)
+        register_button.move(220,190)
+        register_button.setFont(QFont("Segoe UI",11))
+        register_button.clicked.connect(self.registrar_usuario)
+        register_button.setStyleSheet("""
+            QPushButton {
+                background-color: #3c3c3c;    
+                border-radius: 15px;          
+                color: white;                 
+                font-size: 16px;              
+                padding:5px;     
+                border-bottom: 0.5px solid #727272;  
+            
+            }
+            QPushButton:hover {
+                background-color: #424242;   
+            }
+            QPushButton:pressed {
+                background-color: #4E4E4E; 
+                color: #979696;   
+            }
+        """)
+    
+    
+    #def mostrar_contraseña_check(self,cliked):
+        #if cliked:
+            #self.password_input.setEchoMode(
+                #QLineEdit.EchoMode.Normal
+            #)
+        #else:
+             #self.password_input.setEchoMode(
+                #QLineEdit.EchoMode.Password
+             #)
+    
+    def iniciar_mainview(self):
+        pass
+    
+    def registrar_usuario(self):
+        self.new_user_form = RegistroDeUsuario()
+        self.new_user_form.show()
+        
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ventana = Login()
+    sys.exit(app.exec())
